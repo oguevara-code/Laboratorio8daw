12312b3af1b0f99d4412dffa1da386128ae799c3
oguevara-code <oguevara@unsa.edu.pe>
2026-06-03 22:01:36 -0500

    Implementación de inicio de sesion

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def register(request):
-    return render(request, 'register.html')
+
+    if request.method == 'POST':
+
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+
+            user = User.objects.create_user(
+                username=username,
+                password=password1,
+                email=email,
+                first_name=first_name,
+                last_name=last_name
+            )
+
+            user.save()
+
+            return redirect('/')
+        
+    return render(request, 'accounts/register.html')
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        
+    return render(request, "accounts/login.html")
